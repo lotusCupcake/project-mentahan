7d8e276cb7fe7ac2a25294045138ae9bf113e3b1
fikriansari <fikriansari.mfa@gmail.com>
2022-08-11 16:54:17 +0700

    manajemen akun

--- modules/ManajemenAkun/Controllers/ManajemenAkun.php
+++ modules/ManajemenAkun/Controllers/ManajemenAkun.php
@@ -27,55 +27,51 @@ class ManajemenAkun extends BaseController
         $this->validation = \Config\Services::validation();
     }
 
-    // public function index()
-    // {
-    //     $currentPage = $this->request->getVar('page_manajemenAkun') ? $this->request->getVar('page_manajemenAkun') : 1;
-    //     $keyword = $this->request->getVar('keyword');
-    //     if ($keyword) {
-    //         $manajemenAkun = $this->manajemenAkunModel->getManajemenAkunSearch($keyword);
-    //     } else {
-    //         $manajemenAkun = $this->manajemenAkunModel->getManajemenAkun();
-    //     }
-    //     $data = [
-    //         'menu' => $this->fetchMenu(),
-    //         'title' => "Manajemen Akun",
-    //         'breadcrumb' => ['User', 'Manajemen Akun'],
-    //         'manajemenAkun' => $manajemenAkun->paginate($this->numberPage, 'manajemenAkun'),
-    //         'authGroups' =>  $this->authGroupsModel->findAll(),
-    //         'currentPage' => $currentPage,
-    //         'numberPage' => $this->numberPage,
-    //         'pager' => $this->manajemenAkunModel->pager,
-    //         'validation' => \Config\Services::validation(),
-    //     ];
+    public function index()
+    {
+        $currentPage = $this->request->getVar('page_manajemenAkun') ? $this->request->getVar('page_manajemenAkun') : 1;
+        $keyword = $this->request->getVar('keyword');
+        $manajemenAkun = $this->manajemenAkunModel->getManajemenAkun($keyword);
+        $data = [
+            'menu' => $this->fetchMenu(),
+            'title' => "Manajemen Akun",
+            'breadcrumb' => ['User', 'Manajemen Akun'],
+            'manajemenAkun' => $manajemenAkun->paginate($this->numberPage, 'manajemenAkun'),
+            'authGroups' =>  $this->authGroupsModel->findAll(),
+            'currentPage' => $currentPage,
+            'numberPage' => $this->numberPage,
+            'pager' => $manajemenAkun->pager,
+            'validation' => \Config\Services::validation(),
+        ];
 
-    //     return view('Modules\ManajemenAkun\Views\manajemenAkun', $data);
-    // }
+        return view('Modules\ManajemenAkun\Views\manajemenAkun', $data);
+    }
 
-    // public function edit($id)
-    // {
-    //     $url = $this->request->getServer('HTTP_REFERER');
-    //     $rules = [
-    //         'email' => rv('required', ['required' => 'Email Harus Diisi!']),
-    //         'username' => rv('required', ['required' => 'Nama Harus Diisi!']),
-    //         'role' => rv('required', ['required' => 'Role Harus Dipilih!']),
-    //     ];
-    //     if (!$this->validate($rules)) {
-    //         return redirect()->to('/manajemenAkun')->withInput();
-    //     };
+    public function edit($id)
+    {
+        $url = $this->request->getServer('HTTP_REFERER');
+        $rules = [
+            'userEmail' => rv('required', ['required' => 'Email Harus Diisi!']),
+            'userName' => rv('required', ['required' => 'Nama Harus Diisi!']),
+            'userRole' => rv('required', ['required' => 'Role Harus Dipilih!']),
+        ];
+        if (!$this->validate($rules)) {
+            return redirect()->to($url)->withInput();
+        };
 
-    //     $data = array(
-    //         'email' => trim($this->request->getVar('email')),
-    //         'username' => trim($this->request->getVar('username')),
-    //         'active' => trim($this->request->getVar('active')) == null ? 0 : 1,
-    //     );
+        $data = array(
+            'email' => trim($this->request->getVar('userEmail')),
+            'username' => trim($this->request->getVar('userName')),
+            'active' => trim($this->request->getVar('userActive')) == null ? 0 : 1,
+        );
 
-    //     $data_user_group = array('group_id' => trim($this->request->getPost('role')));
+        $data_user_group = array('group_id' => trim($this->request->getPost('userRole')));
 
-    //     if ($this->manajemenAkunModel->update($id, $data)) {
-    //         if ($this->authGroupsUsersModel->update($id, $data_user_group)) {
-    //             session()->setFlashdata('success', 'Data Akun Berhasil Diupdate !');
-    //             return redirect()->to($url);
-    //         }
-    //     }
-    // }
+        if ($this->manajemenAkunModel->update($id, $data)) {
+            if ($this->authGroupsUsersModel->update($id, $data_user_group)) {
+                session()->setFlashdata('success', 'Data Manajemen Akun Berhasil Diupdate !');
+                return redirect()->to($url);
+            }
+        }
+    }
 }
