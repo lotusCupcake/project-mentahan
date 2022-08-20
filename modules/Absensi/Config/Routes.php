9cf0e3c0bf5984b31d3075f20bbba317b57e59bc
fikriansari <fikriansari.mfa@gmail.com>
2022-08-20 15:44:47 +0700

    edit dan hapus absensi

--- modules/Absensi/Config/Routes.php
+++ modules/Absensi/Config/Routes.php
@@ -5,3 +5,5 @@
 */
 $routes->get('absensi', '\Modules\Absensi\Controllers\Absensi::index');
 $routes->post('absensi/tambah', '\Modules\Absensi\Controllers\Absensi::add');
+$routes->put('absensi/ubah/(:num)', '\Modules\Absensi\Controllers\Absensi::edit/$1');
+$routes->delete('absensi/hapus/(:num)', '\Modules\Absensi\Controllers\Absensi::delete/$1');
