31c5c8cea1a9cb3ab26bfdd8ae3dedfd39431624
nougatlolipop <august.ivan20@gmail.com>
2022-09-03 10:17:10 +0700

    fix dashboar dan penjadwalan

--- public/js/tentatif.js
+++ public/js/tentatif.js
@@ -1,3 +1,10 @@
 function checklistTentatif(value) {
+    let dosen;
+    let sesi;
+    let hari;
+    let ta;
+    dosen = value.split(',')[0];
+    sesi = value.split(',')[1];
+    hari = value.split(',')[2];
     console.log(value);
 }
