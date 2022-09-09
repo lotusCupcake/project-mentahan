b3dc19d1149ea39efb46f92c61f5d68b4f223d3a
fikriansari <fikriansari.mfa@gmail.com>
2022-09-09 12:18:12 +0700

    view excel pemetaan jadwal tentatif

--- modules/PemetaanJadwal/Controllers/PemetaanJadwal.php
+++ modules/PemetaanJadwal/Controllers/PemetaanJadwal.php
@@ -19,6 +19,7 @@ class PemetaanJadwal extends BaseController
     protected $dosenModel;
     protected $jenisJadwalModel;
     protected $spreadsheet;
+    protected $ReaderHtml;
 
     public function __construct()
     {
@@ -26,6 +27,7 @@ class PemetaanJadwal extends BaseController
         $this->dosenModel = new DosenModel();
         $this->jenisJadwalModel = new JenisJadwalModel();
         $this->spreadsheet = new Spreadsheet();
+        $this->ReaderHtml = new \PhpOffice\PhpSpreadsheet\Reader\Html();
     }
 
     public function index()
@@ -85,74 +87,88 @@ class PemetaanJadwal extends BaseController
         $keyword = $this->request->getVar('keyword');
         $dosen =  $this->dosenModel->getDataDosen($keyword)->findAll();
         $hari = ['S', 'S', 'R', 'K', 'J'];
+        $jlhHari = count($hari);
         $jadwal = $this->jenisJadwalModel->getTentatif()->get()->getResult();
         $jadwalTentatif = $this->jenisJadwalModel->getJadwalTentatif()->get()->getResult();
         $jadwalPemetaanJadwalSemester = $detailJadwalExist;
+        $span = 0;
 
-        $spreadsheet = new Spreadsheet();
+        $this->spreadsheet = new Spreadsheet();
+        $htmlString = '';
+        $htmlString .= '<table style="border: 1px solid black">';
+        $htmlString .= '<tr>';
+        $htmlString .= '<th rowspan="3" align="center" valign="center"  style="font-weight: bold;border:solid black">No</th>';
+        $htmlString .= '<th rowspan="3" width="550%" align="center" valign="center" style="font-weight: bold;border: solid black" bgcolor="#B7E1CD">Nama Lengkap</th>';
+        foreach ($jadwal as $key => $jdwl) {
+            $htmlString .= '<th colspan="' . $jlhHari * count(getSesiWhere(['jenis_jadwal.jenisJadwalId' => $jdwl->jenisJadwalId])) . '" align="center"  style="font-weight: bold;border:solid black">' . $jdwl->jenisJadwalKode . '</th>';
+            $htmlString .= '<th bgcolor="gray" style="border:solid black"></th>';
+        }
+        foreach ($jadwalTentatif as $key => $tentatif) {
+            $htmlString .= '<th rowspan="3" align="center" valign="center" style="font-weight: bold;border:solid black">' . $tentatif->jenisJadwalKode . '</th>';
+            $htmlString .= '<th rowspan="3" align="center" valign="center" style="font-weight: bold;border:solid black" bgcolor="#B7E1CD">Total</th>';
+        }
+        $htmlString .= '<th rowspan="3" align="center" valign="center" style="font-weight: bold;border:solid black" bgcolor="#B7E1CD">Grand Total</th>';
+        $htmlString .= '</tr>';
+        $htmlString .= '<tr>';
+        foreach ($jadwal as $key => $jdwl) {
+            foreach (getSesiWhere(['jenis_jadwal.jenisJadwalId' => $jdwl->jenisJadwalId]) as $key => $sesi) {
+                $htmlString .= '<th colspan="' . $jlhHari . '" align="center" style="font-weight: bold;border:solid black" bgcolor="#B7E1CD">' . $sesi->sesiStart . '-' . $sesi->sesiEnd . '</th>';
+            }
+            $htmlString .= '<th bgcolor="gray" style="border:solid black"></th>';
+        }
+        $htmlString .= '</tr>';
+        $htmlString .= '<tr>';
+        foreach ($jadwal as $key => $jdwl) {
+            foreach (getSesiWhere(['jenis_jadwal.jenisJadwalId' => $jdwl->jenisJadwalId]) as $key => $sesi) {
+                foreach ($hari as $key => $value) {
+                    $htmlString .= '<th align="center" style="font-weight: bold;border:solid black" bgcolor="#A2C4C9">' . $value . '</th>';
+                }
+            }
+            $htmlString .= '<th bgcolor="gray" style="border:solid black"></th>';
+        }
+        $htmlString .= '</tr>';
+        $no = 1;
+        foreach ($dosen as $i => $data) {
+            $htmlString .= '<tr>';
+            $htmlString .= '<td style="text-align:center;border:solid black">' . $no++ . '</td>';
+            $htmlString .= '<td style="border:solid black">' . $data->dosenFullname . '</td>';
+            foreach ($jadwal as $key => $jdwl) {
+                foreach (getSesiWhere(['jenis_jadwal.jenisJadwalId' => $jdwl->jenisJadwalId]) as $key => $sesi) {
+                    foreach ($hari as $h => $value) {
+                        $htmlString .= '<td align="center" style="border:solid black ">';
+                        if (count($jadwalPemetaanJadwalSemester) > 0) {
+                            foreach ($jadwalPemetaanJadwalSemester as $jad => $dtjadwal) {
+                                if ($dtjadwal['dosen'] == $data->dosenId && $dtjadwal['sesi'] == $sesi->sesiId && in_array($h, array_map('intval', $dtjadwal['hari'])) && $jdwl->unic == $dtjadwal['jenis']) {
+                                    $htmlString .= '1';
+                                }
+                            }
+                        }
+                        $htmlString .= '</td>';
+                    }
+                }
+                $htmlString .= '<td bgcolor="gray" style="border:solid black"></td>';
+            }
+            foreach ($jadwalTentatif as $key => $tentatif) {
+                $htmlString .= '<td align="center" style="border:solid black"></td>';
+                $htmlString .= '<td bgcolor="#B7E1CD" align="center" style="border:solid black"></td>';
+            }
+            $htmlString .= '<td bgcolor="#B7E1CD" align="center" style="border:solid black"></td>';
+            $htmlString .= '</tr>';
+        }
+        $htmlString .= '</table>';
 
-        $default = 1;
-        $konten = 0;
-        $konten = $default + $konten;
-        $spreadsheet->setActiveSheetIndex(0)
-            ->setCellValue('A' . $konten, 'No.')
-            ->setCellValue('B' . $konten, 'Jenis Kegiatan')
-            ->setCellValue('C' . $konten, 'Nilai (Bobot X Nilai)')->getStyle("A" . $konten . ":" . "C" . $konten)->getFont()->setBold(true);
-        $konten++;
-        $writer = new Xlsx($spreadsheet);
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+        $writer = new \PhpOffice\PhpSpreadsheet\Writer\Html($this->spreadsheet);
+        $this->spreadsheet = $reader->loadFromString($htmlString);
+
+        $writer = new Xlsx($this->spreadsheet);
         $fileName = 'Tentatif Jadwal Tahun Ajaran ' . $tahunAjaran;
-
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename=' . $fileName . '.xlsx');
-        header('Cache-Control: max-age=0');
-
-        $writer->save('php://output');
-        exit();
-
-        // $htmlString =
-        //     '<table>
-        //           <tr>
-        //               <td>Hello World</td>
-        //           </tr>
-        //           <tr>
-        //               <td>Hello<br />World</td>
-        //           </tr>
-        //           <tr>
-        //               <td>Hello<br>World</td>
-        //           </tr>
-        //       </table>';
-
-        // $reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
-        // $spreadsheet = $reader->loadFromString($htmlString);
-        // $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
-        // $spreadsheet = new Spreadsheet();
-        // $sheet = $spreadsheet->getActiveSheet();
-        // $sheet->setCellValue('A1', 'Hello World !');
-        // $fileName = 'Tentatif Jadwal Tahun Ajaran ' . $tahunAjaran;
-        // $writer = new Xlsx($spreadsheet);
-        // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        // header('Content-Disposition: attachment;filename=' . $fileName . '.xlsx');
-        // header('Cache-Control: max-age=0');
-
-        // $writer->setOffice2003Compatibility(true);
-        // ob_end_clean();
-        // $writer->save('php://output');
-        // exit();
-    }
-
-    public function test()
-    {
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'Hello World !');
-
-        $writer = new Xlsx($spreadsheet);
         ob_end_clean();
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="myfile.xlsx"');
+        header('Content-Disposition: attachment;filename="' . $fileName . '.xlsx"');
         header('Cache-Control: max-age=0');
 
+        ob_end_clean();
         $writer->save('php://output');
-        exit();
     }
 }
